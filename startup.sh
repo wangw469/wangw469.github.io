c47412a650d4b40f8d665595a3964d7b58b91444
wen <wen@trackview.co>
2019-07-27 22:47:43 +0800

    update startup scirpt
    TEST: ./startup.sh

--- startup.sh
+++ startup.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 sudo docker stop wangw469.github.io
 
-sudo docker run --rm --name wangw469.github.io \
- -p 4000:4000 \
- -v "$(pwd)":/usr/src/jekyll\
- -w /usr/src/jekyll\
- --volume="$HOME/.gem_cache:/usr/local/bundle" \
- --detach=true \
- jekyll/jekyll \
- bundle exec jekyll serve --host "0.0.0.0"
+export JEKYLL_VERSION=3.8
+docker run --rm --name wangw469.github.io \
+  -p 4000:4000 \
+  --volume="$PWD:/srv/jekyll" \
+  -v "/Users/xp/.gem_cache:/usr/local/bundle" \
+  -it jekyll/jekyll:$JEKYLL_VERSION \
+  jekyll server --host "0.0.0.0"
+
