ceb7ffbf5cbbb9e5a2f1956bdf176810231dd32a
wangw469 <wangw469@gmail.com>
2026-04-05 12:59:35 +0800

    add song tag plugin

---
+++ _plugins/song_tag.rb
@@ -0,0 +1,50 @@
+module Jekyll
+  class SongTag < Liquid::Tag
+    def initialize(tag_name, markup, tokens)
+      super
+      @artist = ""
+      @title = ""
+      parse_markup(markup)
+    end
+
+    def parse_markup(markup)
+      # Support two formats:
+      # {% song artist="Eason Chan" title="富士山下" %}
+      # {% song "Eason Chan" "富士山下" %}
+      
+      if markup.include?("=")
+        # Named parameters format
+        markup.scan(/(\w+)\s*=\s*"([^"]*)"/) do |key, value|
+          @artist = value if key == "artist"
+          @title = value if key == "title"
+        end
+      else
+        # Positional parameters format
+        parts = markup.scan(/"([^"]*)"/)
+        @artist = parts[0][0] if parts[0]
+        @title = parts[1][0] if parts[1]
+      end
+    end
+
+    def render(context)
+      query = URI.encode_www_form_component("#{@artist} #{@title}")
+      artist_encoded = URI.encode_www_form_component(@artist)
+      title_encoded = URI.encode_www_form_component(@title)
+      
+      <<~HTML
+        <div class="song-link">
+          <span class="song-info">🎵 #{@artist} - #{@title}</span>
+          <span class="song-services">
+            <a href="https://open.spotify.com/search/#{query}" target="_blank" rel="noopener" title="Spotify">Spotify</a>
+            <a href="https://music.apple.com/search?term=#{query}" target="_blank" rel="noopener" title="Apple Music">Apple Music</a>
+            <a href="https://music.youtube.com/search?q=#{query}" target="_blank" rel="noopener" title="YouTube Music">YouTube Music</a>
+            <a href="https://music.163.com/#/search?keyword=#{query}" target="_blank" rel="noopener" title="NetEase Cloud Music">NetEase</a>
+            <a href="https://y.qq.com/n/ryqq/search?w=#{query}" target="_blank" rel="noopener" title="QQ Music">QQ Music</a>
+          </span>
+        </div>
+      HTML
+    end
+  end
+end
+
+Liquid::Template.register_tag("song", Jekyll::SongTag)
---
+++ _posts/2026-04-05-testing-song-tag-plugin.md
@@ -0,0 +1,17 @@
+---
+layout: post
+title: "Testing Song Tag Plugin"
+date: 2026-04-05
+---
+
+Here are some of my favorite songs:
+
+{% song artist="Eason Chan" title="富士山下" %}
+
+{% song artist="Jay Chou" title="晴天" %}
+
+{% song artist="Taylor Swift" title="Love Story" %}
+
+You can also use the positional format:
+
+{% song "Faye Wong" "红豆" %}
